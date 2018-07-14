cbbfe558c00a32b312916844d88f101b97b9e503
Tiger <cia.fbi.007@hotmail.com>
2018-07-14 17:39:12 +0800

    修改 loader 代码，实现分片加载数据

--- src/scripts/Loader.ts
+++ src/scripts/Loader.ts
@@ -1,3 +1,40 @@
 import WorkerAdaptor from './WorkerAdaptor';
 
 const adaptor = new WorkerAdaptor(self as DedicatedWorkerGlobalScope);
+adaptor.on('start', start);
+
+let contentFragment = '';
+const decoder = new TextDecoder();
+
+function start() {
+  fetch('sample_docs/001.txt', { mode: 'no-cors' })
+    .then((res) => consume(res.body.getReader()))
+    .catch((e) => console.log("something went wrong: " + e));
+}
+
+function consume(reader: ReadableStreamReader) {
+  return reader.read().then(function processChunk({ done, value }): Promise<any> {
+    if (done) {
+      chunkFinished();
+      return;
+    }
+    chunkTemp(value);
+    return reader.read().then(processChunk);
+  });
+}
+
+function chunkTemp(value: Uint8Array) {
+  contentFragment += decoder.decode(value, {stream: true});
+  const contentArray: string[] = contentFragment.split(',\n');
+  contentArray.forEach((content, index) => {
+    if (index === contentArray.length - 1) {
+      contentFragment = contentFragment.substr(contentFragment.length - content.length);
+    } else {
+      adaptor.send('append', JSON.parse(content));
+    }
+  });
+}
+
+function chunkFinished() {
+  adaptor.send('append', JSON.parse(contentFragment));
+}
