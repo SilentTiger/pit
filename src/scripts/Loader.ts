b539f5e2598d707e33e78a55c3d43cad9a3feb2b
Tiger <cia.fbi.007@hotmail.com>
2018-12-27 16:35:19 +0800

    修改数据加载逻辑

--- src/scripts/Loader.ts
+++ src/scripts/Loader.ts
@@ -1,13 +1,19 @@
+/**
+ * Loader 负责加载 sample_docs 中的数据文件，同时将数据文件中的字符数据转换成 JSON 数据，并发回给主线程
+ * 注意，加载、转换和发送的过程是并行的
+ */
+
+import {MessageEnum} from './MessageEnum';
 import WorkerAdaptor from './WorkerAdaptor';
 
 const adaptor = new WorkerAdaptor(self as DedicatedWorkerGlobalScope);
-adaptor.on('start', start);
+adaptor.on(MessageEnum.startLoad, start);
 
 let contentFragment = '';
 let decoder = new TextDecoder();
 
 function start() {
-  fetch('sample_docs/001.txt', { mode: 'no-cors' })
+  fetch('sample_docs/002.txt', { mode: 'no-cors' })
     .then((res) => consume(res.body.getReader()))
     .catch((e) => console.log("something went wrong: " + e));
 }
@@ -25,18 +31,18 @@ function consume(reader: ReadableStreamReader) {
 
 function chunkTemp(value: Uint8Array) {
   contentFragment += decoder.decode(value, {stream: true});
-  const contentArray: string[] = contentFragment.split(',\n');
+  const contentArray: string[] = contentFragment.split('\n');
   contentArray.forEach((content, index) => {
     if (index === contentArray.length - 1) {
       contentFragment = contentFragment.substr(contentFragment.length - content.length);
     } else {
-      adaptor.send('append', JSON.parse(content));
+      adaptor.send(MessageEnum.LoaderAppend, JSON.parse(content));
     }
   });
 }
 
 function chunkFinished() {
-  adaptor.send('append', JSON.parse(contentFragment));
+  adaptor.send(MessageEnum.LoaderEnd, JSON.parse(contentFragment));
   // 为以防万一，最后一段数据处理完之后重置存储临时内容的变量和解码器，以免接收到的数据有问题，导致数据有残留
   contentFragment = '';
   decoder = new TextDecoder();
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,21 +1,30 @@
 import Logger from './Log';
+import {MessageEnum} from './MessageEnum';
 
 Logger.info('hello');
 Logger.error('world');
 
+import Constructor from 'worker-loader!./Constructor';
 import Loader from 'worker-loader!./Loader';
 const loader = new Loader();
+const constructor = new Constructor();
 
 setTimeout(() => {
-  console.time('cost');
-  loader.postMessage({ cmd: 'start' });
-  let count = 0;
-  loader.onmessage = (event) => {
-    if (event.data.cmd === 'append') {
-      count++;
-    }
-    if (count === 11180) {
-      console.timeEnd('cost');
-    }
-  };
+  constructor.postMessage({ cmd: MessageEnum.startConstruct }); // 启动 constructor
+  loader.postMessage({ cmd: MessageEnum.startLoad });           // 启动 loader
 }, 2000);
+
+loader.onmessage = (event) => {
+  if (event.data.cmd === MessageEnum.LoaderAppend) {
+    constructor.postMessage({cmd: MessageEnum.appendConstruct, data: event.data.data}); // 给 constructor 添加数据
+  }
+  if (event.data.cmd === MessageEnum.LoaderEnd) {
+    constructor.postMessage({cmd: MessageEnum.endConstruct, data: event.data.data});    // 通知 constructor 结束
+  }
+};
+
+constructor.onmessage = (event) => {
+  if (event.data.cmd === MessageEnum.ConstrustorEnd) {
+    console.log('final document data ', event.data);
+  }
+};
