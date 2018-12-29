d969e8101d03eec4f667018ce2d2f1f168b2406e
Tiger <cia.fbi.007@hotmail.com>
2018-12-29 21:13:26 +0800

    完善图片片段的属性

--- src/scripts/DocStructure/FragmentImageAttributes.ts
+++ src/scripts/DocStructure/FragmentImageAttributes.ts
@@ -6,12 +6,16 @@ export default class FragmentImageAttributes extends FragmentAttributes {
   public height: number;
   public layout: EnumLayout;
   public margin: number;
+  public oriHeight: number;
+  public oriWidth: number;
 }
 
 const FragmentImageDefaultAttributes = {
   height: 0,
   layout: EnumLayout.block,
   margin: 0,
+  oriHeight: 0,
+  oriWidth: 0,
   width: 0,
 };
 
