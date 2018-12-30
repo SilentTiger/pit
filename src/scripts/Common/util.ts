6733e7fa6e66f8529271d987349c871064a1fc64
Tiger <cia.fbi.007@hotmail.com>
2018-12-30 18:49:26 +0800

    添加生成 guid 的工具方法

---
+++ src/scripts/Common/util.ts
@@ -0,0 +1,17 @@
+export const guid = (() => {
+  const pool = new Set();
+  const generate = () => {
+    return Math.floor((1 + Math.random()) * 0x1000000)
+    .toString(16)
+    .substring(1);
+
+  };
+  return () => {
+    let current = generate();
+    while (pool.has(current)) {
+      current = generate();
+    }
+    pool.add(current);
+    return current;
+  };
+})();
