87c088a26bcd18694c463d2b06c5cc39a54664b7
Tiger <cia.fbi.007@hotmail.com>
2018-12-30 18:27:27 +0800

    初始化渲染逻辑结构

---
+++ src/scripts/RenderStructure/Frame.ts
@@ -0,0 +1,3 @@
+import { LinkedList } from "../Common/LinkedList";
+import Line from "./Line";
+export default class Frame extends LinkedList<Line> {}
---
+++ src/scripts/RenderStructure/LayoutTree.ts
@@ -0,0 +1,3 @@
+import { LinkedList } from "../Common/LinkedList";
+import Frame from "./Frame";
+export default class LayoutTree extends LinkedList<Frame> {}
