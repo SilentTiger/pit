b5b4bd1e81078ea43ae11ece0b7535903ce2868f
Tiger <cia.fbi.007@hotmail.com>
2018-12-31 23:13:14 +0800

    添加一点点生成 Frame 的逻辑

--- src/scripts/RenderStructure/Line.ts
+++ src/scripts/RenderStructure/Line.ts
@@ -1,8 +1,19 @@
+import IRectangle from '../Common/IRectangle';
 import { ILinkedListNode, LinkedList } from "../Common/LinkedList";
 import Frame from "./Frame";
 import Run from "./Run";
-export default class Line extends LinkedList<Run> implements ILinkedListNode {
+export default class Line extends LinkedList<Run> implements ILinkedListNode, IRectangle {
+  public x: number;
+  public y: number;
+  public width: number;
+  public height: number;
   public prevSibling: Line;
   public nextSibling: Line;
   public parent: Frame;
+
+  constructor(x: number, y: number) {
+    super();
+    this.x = x;
+    this.y = y;
+  }
 }
--- src/scripts/RenderStructure/Root.ts
+++ src/scripts/RenderStructure/Root.ts
@@ -1,10 +1,21 @@
+import IRectangle from '../Common/IRectangle';
 import { LinkedList } from "../Common/LinkedList";
 import Document from '../DocStructure/Document';
 import Paragraph from '../DocStructure/Paragraph';
 import Frame from "./Frame";
-export default class Root extends LinkedList<Frame> {
-  constructor(data: Document) {
+export default class Root extends LinkedList<Frame> implements IRectangle {
+  public x: number;
+  public y: number;
+  public width: number;
+  public height: number;
+  constructor(data: Document, x?: number, y?: number) {
     super();
+    if (x !== undefined) {
+      this.x = x;
+    }
+    if (y !== undefined) {
+      this.y = y;
+    }
     const current = data.head;
     while (current) {
       this.addParagraph(current);
@@ -12,7 +23,11 @@ export default class Root extends LinkedList<Frame> {
   }
 
   public addParagraph(paragraph: Paragraph) {
-    const frame = new Frame(paragraph);
+    let pY = 0;
+    if (this.tail !== null) {
+      pY = this.tail.y + this.tail.height;
+    }
+    const frame = new Frame(paragraph, this.x, pY);
     this.add(frame);
   }
 }
