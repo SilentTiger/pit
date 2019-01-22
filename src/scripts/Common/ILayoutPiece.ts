8b1794407535a6a36e77fa00699b3fd8004d526a
Tiger <cia.fbi.007@hotmail.com>
2019-01-22 14:58:28 +0800

    把 layout piece 改为类

--- src/scripts/Common/ILayoutPiece.ts
+++
@@ -1,5 +0,0 @@
-export default interface ILayoutPiece {
-  isSpace: boolean;
-  text: string;
-  width?: number;   // 某些情况下，排版逻辑不依赖于 ILayoutPiece 的宽度，此时可以不设置这个字段
-}
---
+++ src/scripts/Common/LayoutPiece.ts
@@ -0,0 +1,62 @@
+import Fragment from "../DocStructure/Fragment";
+import FragmentText from "../DocStructure/FragmentText";
+import { measureTextWidth } from "./Platform";
+
+export default class LayoutPiece {
+  public frags: Array<{
+    start?: number;
+    end?: number;
+    frag: Fragment;
+  }>;
+  public isSpace?: boolean;
+  public text?: string;
+  public totalWidth?: number;
+  public fragWidth?: number[];
+  public isHolder: boolean;
+
+  public calTotalWidth() {
+    if (this.isHolder) {
+      this.totalWidth = this.frags[0].frag.calSize().width;
+      this.fragWidth = [this.totalWidth];
+      return;
+    }
+    let width: number;
+    if (this.frags.length === 1) {
+      width = measureTextWidth(
+        this.text,
+        (this.frags[0].frag as FragmentText).attributes,
+      );
+    } else {
+      if (this.fragWidth === undefined || !this.fragWidth.length) {
+        this.calFragWidth();
+      }
+      width = this.fragWidth.reduce((sum, cur) => {
+        return sum + cur;
+      }, 0);
+    }
+    this.totalWidth = width;
+  }
+
+  public calFragWidth() {
+    if (this.frags.length === 1) {
+      this.fragWidth = [this.totalWidth];
+    } else {
+      this.fragWidth = this.frags.map((frag) => {
+        return measureTextWidth(
+          this.text.substring(frag.start, frag.end),
+          (frag.frag as FragmentText).attributes,
+        );
+      });
+    }
+  }
+
+  public calCharWidthByFrag(fragIndex: number) {
+    const frag = this.frags[fragIndex];
+    return this.text
+      .substr(frag.start, frag.end)
+      .split("")
+      .map((char) => {
+        return measureTextWidth(char, (frag.frag as FragmentText).attributes);
+      });
+  }
+}
