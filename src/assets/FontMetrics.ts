64ec4571c23a0adfde4b1f52c2f4759826120b8d
Tiger <cia.fbi.007@hotmail.com>
2019-01-25 14:41:39 +0800

    把 font metrics 改为 ts

--- src/assets/FontMetrics.ts
+++ src/assets/FontMetrics.ts
@@ -2,10 +2,9 @@
 // Variables
 // ——————————————————————————————————————————————————
 
-let initialized = false
-let padding
-let context
-let canvas
+let padding: number;
+let context: CanvasRenderingContext2D;
+let canvas: HTMLCanvasElement;
 
 // ——————————————————————————————————————————————————
 // Settings
@@ -18,88 +17,91 @@ const settings = {
     xHeight: 'x',
     descent: 'p',
     ascent: 'h',
-    tittle: 'i'
-  }
-}
+    tittle: 'i',
+  },
+};
 
 // ——————————————————————————————————————————————————
 // Methods
 // ——————————————————————————————————————————————————
 
 const initialize = () => {
-  canvas = document.createElement('canvas')
-  context = canvas.getContext('2d')
-  initialized = true
-}
+  canvas = document.createElement('canvas');
+  context = canvas.getContext('2d');
+};
 
-const setFont = (fontFamily, fontSize, fontWeight) => {
-  if (!initialized) initialize()
-  padding = fontSize * 0.5
-  canvas.width = fontSize * 2
-  canvas.height = fontSize * 2 + padding
-  context.font = `${fontWeight} ${fontSize}px ${fontFamily}`
-  context.textBaseline = 'top'
-  context.textAlign = 'center'
-}
+const setFont = (fontFamily: string, fontSize: number, fontWeight: string) => {
+  padding = fontSize * 0.5;
+  canvas.width = fontSize * 2;
+  canvas.height = fontSize * 2 + padding;
+  context.font = `${fontWeight} ${fontSize}px ${fontFamily}`;
+  context.textBaseline = 'top';
+  context.textAlign = 'center';
+};
 
-const setAlignment = (baseline = 'top') => {
-  const ty = baseline === 'bottom' ? canvas.height : 0
-  context.setTransform(1, 0, 0, 1, 0, ty)
-  context.textBaseline = baseline
-}
+const setAlignment = (baseline: CanvasTextBaseline = 'top') => {
+  const ty = baseline === 'bottom' ? canvas.height : 0;
+  context.setTransform(1, 0, 0, 1, 0, ty);
+  context.textBaseline = baseline;
+};
 
-const updateText = (text) => {
-  context.clearRect(0, 0, canvas.width, canvas.height)
-  context.fillText(text, canvas.width / 2, padding, canvas.width)
-}
+const updateText = (text: string) => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillText(text, canvas.width / 2, padding, canvas.width);
+};
 
-const computeLineHeight = () => {
-  const letter = 'A'
-  setAlignment('bottom')
-  const gutter = canvas.height - measureBottom(letter)
-  setAlignment('top')
-  return measureBottom(letter) + gutter
-}
+const computeLineHeight = (): number => {
+  const letter = 'A';
+  setAlignment('bottom');
+  const gutter = canvas.height - measureBottom(letter);
+  setAlignment('top');
+  return measureBottom(letter) + gutter;
+};
 
-const getPixels = (text) => {
-  updateText(text)
-  return context.getImageData(0, 0, canvas.width, canvas.height).data
-}
+const getPixels = (text: string) => {
+  updateText(text);
+  return context.getImageData(0, 0, canvas.width, canvas.height).data;
+};
 
-const getFirstIndex = (pixels) => {
+const getFirstIndex = (pixels: Uint8ClampedArray) => {
   for (let i = 3, n = pixels.length; i < n; i += 4) {
-    if (pixels[i] > 0) return (i - 3) / 4
-  } return pixels.length
-}
+    if (pixels[i] > 0) { return (i - 3) / 4; }
+  }
+  return pixels.length;
+};
 
-const getLastIndex = (pixels) => {
+const getLastIndex = (pixels: Uint8ClampedArray) => {
   for (let i = pixels.length - 1; i >= 3; i -= 4) {
-    if (pixels[i] > 0) return i / 4
-  } return 0
-}
+    if (pixels[i] > 0) { return i / 4; }
+  }
+  return 0;
+};
 
-const normalize = (metrics, fontSize, origin) => {
-  const result = {}
-  const offset = metrics[origin]
-  for (let key in metrics) { result[key] = (metrics[key] - offset) / fontSize }
-  return result
-}
+const normalize = (metrics: { [key: string]: number }, fontSize: number, origin: CanvasTextBaseline ) => {
+  const result: { [key: string]: number } = {};
+  const offset = metrics[origin];
+  // tslint:disable-next-line:forin
+  for (const key in metrics) {
+    result[key] = (metrics[key] - offset) / fontSize;
+  }
+  return result;
+};
 
-const measureTop = (text) => (
+const measureTop = (text: string): number => (
   Math.round(
     getFirstIndex(
-      getPixels(text)
-    ) / canvas.width
+      getPixels(text),
+    ) / canvas.width,
   ) - padding
-)
+);
 
-const measureBottom = (text) => (
+const measureBottom = (text: string): number => (
   Math.round(
     getLastIndex(
-      getPixels(text)
-    ) / canvas.width
+      getPixels(text),
+    ) / canvas.width,
   ) - padding
-)
+);
 
 const getMetrics = (chars = settings.chars) => ({
   capHeight: measureTop(chars.capHeight),
@@ -109,8 +111,8 @@ const getMetrics = (chars = settings.chars) => ({
   bottom: computeLineHeight(),
   ascent: measureTop(chars.ascent),
   tittle: measureTop(chars.tittle),
-  top: 0
-})
+  top: 0,
+});
 
 // ——————————————————————————————————————————————————
 // FontMetrics
@@ -120,20 +122,20 @@ const FontMetrics = ({
   fontFamily = 'Times',
   fontWeight = 'normal',
   fontSize = 200,
-  origin = 'baseline'
+  origin = 'baseline',
 } = {}) => (
   setFont(fontFamily, fontSize, fontWeight), {
     ...normalize(getMetrics(), fontSize, origin),
     fontFamily,
     fontWeight,
-    fontSize
+    fontSize,
   }
-)
+);
 
-FontMetrics.settings = settings
+FontMetrics.settings = settings;
 
 // ——————————————————————————————————————————————————
 // Exports
 // ——————————————————————————————————————————————————
-
-export default FontMetrics
+initialize();
+export default FontMetrics;
