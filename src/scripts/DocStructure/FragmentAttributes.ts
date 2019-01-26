449083701e29fa8a929f57710b3d68f14caf8bff
Tiger <cia.fbi.007@hotmail.com>
2019-01-26 22:35:13 +0800

    把背景色属性挪到 fragment 基类属性中去

--- src/scripts/DocStructure/FragmentAttributes.ts
+++ src/scripts/DocStructure/FragmentAttributes.ts
@@ -1,10 +1,12 @@
 export default class FragmentAttributes {
   public author: string;
+  public background: string;
   public comment: string;
 }
 
 const FragmentDefaultAttributes = {
   author: '',
+  background: '#ffffff',
   comment: '',
 };
 
