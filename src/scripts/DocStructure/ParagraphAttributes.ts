c8a866a40fbe6da559568a1942f24b87c00afebd
Tiger <cia.fbi.007@hotmail.com>
2019-02-01 12:48:50 +0800

    修改属性位置

--- src/scripts/DocStructure/ParagraphAttributes.ts
+++ src/scripts/DocStructure/ParagraphAttributes.ts
@@ -2,12 +2,14 @@ import { EnumAlign } from "./EnumParagraphStyle";
 
 export default class ParagraphAttributes {
   public align: EnumAlign;
+  public blockquote: boolean;
   public linespacing: number;
   public indent: number;
 }
 
 const ParagraphDefaultAttributes: ParagraphAttributes = {
   align: EnumAlign.justify,
+  blockquote: false,
   indent: 0,
   linespacing: 1.7,
 };
