9165d3a09fde852d60aa3b9fd70b899f0ee45346
Tiger <cia.fbi.007@hotmail.com>
2019-03-11 13:33:42 +0800

    完成分割线排版绘制逻辑

--- src/scripts/DocStructure/Divide.ts
+++ src/scripts/DocStructure/Divide.ts
@@ -1,15 +1,23 @@
+import ICanvasContext from "../Common/ICanvasContext";
 import Block from "./Block";
 
 export default class Divide extends Block {
-  public height = 45;
-
-  public layout(): boolean {
+  public layout() {
     this.needLayout = false;
-    return false;
+    if (this.height !== 45) {
+      this.setSize({ height: 45 });
+      if (this.nextSibling !== null) {
+        this.nextSibling.setPositionY(this.y + this.height);
+      }
+    }
   }
 
-  protected render(ctx: import("../Common/ICanvasContext").default): void {
-    throw new Error("Method not implemented.");
+  protected render(ctx: ICanvasContext): void {
+    ctx.strokeStyle = '#41464b';
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y + 22);
+    ctx.lineTo(this.x + 616, this.y + 22);
+    ctx.stroke();
   }
-
 }
