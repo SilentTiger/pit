ae98426c2a781d0cb10885485f3cca7d5c147a22
Tiger <cia.fbi.007@hotmail.com>
2019-04-02 19:16:48 +0800

    添加 cover canvas

--- src/scripts/Common/ICanvasContext.ts
+++ src/scripts/Common/ICanvasContext.ts
@@ -1,3 +1,6 @@
+import IRectangle from "./IRectangle";
+
 export default interface ICanvasContext extends CanvasRenderingContext2D {
   drawCursor(x: number, y: number, height: number, color: string): void;
+  drawSelectionArea(rects: IRectangle[], scrollTop: number): void;
 }
