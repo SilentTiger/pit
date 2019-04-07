c4c30e2c8be9e60509e94f9348dda59fc8b30b49
Tiger <cia.fbi.007@hotmail.com>
2019-04-07 12:08:25 +0800

    把 color 属性挪到 fragment attributes 上

--- src/scripts/DocStructure/FragmentAttributes.ts
+++ src/scripts/DocStructure/FragmentAttributes.ts
@@ -1,6 +1,7 @@
 export default interface IFragmentAttributes {
   author: string;
   background: string;
+  color: string;
   comment: string;
   strike: boolean;
   underline: boolean;
@@ -9,6 +10,7 @@ export default interface IFragmentAttributes {
 const FragmentDefaultAttributes = {
   author: '',
   background: '#ffffff',
+  color: '#494949',
   comment: '',
   strike: false,
   underline: false,
