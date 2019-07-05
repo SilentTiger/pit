41d9ebc8bac48a787b6f60ff195ab0a9a3a89723
Tiger <cia.fbi.007@hotmail.com>
2019-07-05 15:11:40 +0800

    关闭单行代码最大长度限制

--- src/scripts/DocStructure/EnumTextStyle.ts
+++ src/scripts/DocStructure/EnumTextStyle.ts
@@ -11,7 +11,6 @@ enum EnumTitle {
 }
 
 const enumFont = new Map();
-// tslint:disable-next-line: max-line-length
 enumFont.set('Default', '-apple-system,BlinkMacSystemFont,"PingFang SC",Helvetica,Tahoma,Arial,"Hiragino Sans GB","Microsoft YaHei","\\5FAE\8F6F\96C5\9ED1",sans-serif');
 enumFont.set('simsun', 'SimSun,STSong,sans-serif');
 enumFont.set('simhei', 'SimHei,STHeiti,sans-serif');
