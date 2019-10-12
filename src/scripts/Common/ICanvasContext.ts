f55fe6d77a5a1b847259ac6a1c1a66bdedbccf58
Tiger <cia.fbi.007@hotmail.com>
2019-10-12 12:00:04 +0800

    完善搜索与绘制之间的关系以及优化绘制搜索结果的性能

--- src/scripts/Common/ICanvasContext.ts
+++ src/scripts/Common/ICanvasContext.ts
@@ -4,5 +4,5 @@ import { ISearchResult } from "./ISearchResult";
 export default interface ICanvasContext extends CanvasRenderingContext2D {
   drawCursor(x: number, y: number, height: number, color: string): void;
   drawSelectionArea(rects: IRectangle[], scrollTop: number): void;
-  drawSearchResult(results: ISearchResult[], scrollTop: number, currentIndex?: number): void;
+  drawSearchResult(results: ISearchResult[], scrollTop: number, viewEnd: number, currentIndex?: number): void;
 }
