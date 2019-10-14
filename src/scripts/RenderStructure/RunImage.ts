3a28ed02a40a3e99833efaa164e39ca629f32b33
Tiger <cia.fbi.007@hotmail.com>
2019-10-14 15:11:34 +0800

    修复图片不能绘制的问题

--- src/scripts/RenderStructure/RunImage.ts
+++ src/scripts/RenderStructure/RunImage.ts
@@ -10,22 +10,37 @@ export default class RunImage extends Run {
     this.frag = frag;
   }
 
+  /**
+   * 绘制当前图片
+   */
   public draw(ctx: ICanvasContext, x: number, y: number): void {
-    if (this.frag.img.complete) {
+    if (this.frag.img.src) {
       ctx.drawImage(this.frag.img, this.x + x, this.y + y, this.frag.attributes.width, this.frag.attributes.height);
     } else {
+      this.frag.setImage();
       this.frag.img.onload = () => {
         ctx.drawImage(this.frag.img, this.x + x, this.y + y, this.frag.attributes.width, this.frag.attributes.height);
       };
     }
   }
+
+  /**
+   * 计算高度
+   */
   public calHeight(): number {
     return this.frag.attributes.height;
   }
+
+  /**
+   * 计算宽度
+   */
   public calWidth(): number {
     return this.frag.attributes.width;
   }
 
+  /**
+   * 获取坐标在文档中的位置
+   */
   public getDocumentPos(x: number, y: number, tryHead?: boolean): number {
     if (x < this.width / 2) {
       return tryHead ? 0 : -1;
