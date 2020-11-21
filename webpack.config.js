f9c64d88d8cb2874761041f0aa487f9a2e6d4d7c
SilentTiger <cia.fbi.007@hotmail.com>
2020-11-21 18:21:01 +0800

    更新依赖项版本

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const os = require('os')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
@@ -26,6 +25,9 @@ const webpackConfig = {
     contentBase: path.resolve(__dirname, 'src/assets'),
   },
   mode: 'development',
+  cache: {
+    type: 'filesystem'
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/assets/template.ejs',
@@ -80,17 +82,15 @@ if (process.env.NODE_ENV === 'demo') {
   webpackConfig.optimization = {
     minimize: true,
     minimizer: [new TerserPlugin({
-      sourceMap: false,
       parallel: os.cpus().length,
     })],
   }
   webpackConfig.mode = 'production'
+  webpackConfig.cache = false
   delete webpackConfig.devtool
   webpackConfig.plugins.unshift(new CopyPlugin({ patterns: [{ from: 'src/assets/sample_docs', to: '../dist/sample_docs' }] }))
   webpackConfig.plugins.unshift(new CleanWebpackPlugin())
   // webpackConfig.plugins.unshift(new BundleAnalyzerPlugin())
-} else {
-  webpackConfig.plugins.unshift(new HardSourceWebpackPlugin())
 }
 
 module.exports = webpackConfig
