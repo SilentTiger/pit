0a5099c45f8dd344ffc661fce03a92bb8a0a3420
SilentTiger <cia.fbi.007@hotmail.com>
2021-04-04 16:42:19 +0800

    更新依赖项版本

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,3 @@
-module.exports = ({ file, options, env }) => ({
-  parser: file.extname === '.scss' ? 'postcss-scss' : false,
-  plugins: {
-    'postcss-import': {},
-    'postcss-cssnext': {},
-    cssnano: env === 'production' ? {} : false,
-  },
-})
+module.exports = {
+  plugins: ['postcss-preset-env'],
+}
