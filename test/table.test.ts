a5a560d1f6aef4c9d7508ab731b869759ee97c16
SilentTiger <cia.fbi.007@hotmail.com>
2021-05-27 20:06:23 +0800

    chore: 添加单元测试

--- test/table.test.ts
+++ test/table.test.ts
@@ -160,4 +160,90 @@ describe('correctSelectionPos', () => {
       expect(compareDocPos(newSelection2[0].end!, { index: 0, inner: { index: 1, inner: null } })).toBe(0)
     }
   })
+
+  // 从表格中某个单元格开始，一直选到表格后面
+  test('select from table cell to back of table', () => {
+    const delta = new Delta()
+    delta.insert('01')
+    delta.insert(1, { frag: 'end', block: 'para' })
+
+    delta.insert(
+      createTableDelta([
+        ['a', 'b'],
+        ['c', 'de'],
+      ]),
+      { block: 'table' },
+    )
+
+    delta.insert('ab')
+    delta.insert(1, { frag: 'end', block: 'para' })
+    const doc = new Document()
+    doc.readFromChanges(delta)
+    expect(doc.children.length).toBe(3)
+    expect(doc.children[1] instanceof Table).toBe(true)
+
+    if (doc.children[1] instanceof Table) {
+      const table: Table = doc.children[1] as Table
+      const start1 = { index: 0, inner: { index: 1, inner: { index: 0, inner: { index: 1, inner: null } } } }
+      const end1 = null
+      const newSelection1 = table.correctSelectionPos(start1, end1)
+
+      expect(newSelection1.length).toBe(1)
+      expect(compareDocPos(newSelection1[0].start!, { index: 0, inner: { index: 1, inner: null } })).toBe(0)
+      expect(newSelection1[0].end).toBe(null)
+
+      const start2 = { index: 0, inner: { index: 0, inner: { index: 0, inner: { index: 1, inner: null } } } }
+      const end2 = null
+      const newSelection2 = table.correctSelectionPos(start2, end2)
+
+      expect(newSelection2.length).toBe(1)
+      expect(compareDocPos(newSelection2[0].start!, { index: 0, inner: null })).toBe(0)
+      expect(newSelection2[0].end).toBe(null)
+    }
+  })
+
+  // 在同一行中垮单元格选中
+  test('select from cell to cell in one row', () => {
+    const delta = new Delta()
+    delta.insert('01')
+    delta.insert(1, { frag: 'end', block: 'para' })
+
+    delta.insert(
+      createTableDelta([
+        ['aa', 'bb', 'cc'],
+        ['dd', 'ee', 'ff'],
+      ]),
+      { block: 'table' },
+    )
+
+    delta.insert('ab')
+    delta.insert(1, { frag: 'end', block: 'para' })
+    const doc = new Document()
+    doc.readFromChanges(delta)
+    expect(doc.children.length).toBe(3)
+    expect(doc.children[1] instanceof Table).toBe(true)
+
+    if (doc.children[1] instanceof Table) {
+      const table: Table = doc.children[1] as Table
+      const start1 = { index: 0, inner: { index: 0, inner: { index: 0, inner: { index: 1, inner: null } } } }
+      const end1 = { index: 0, inner: { index: 0, inner: { index: 1, inner: { index: 1, inner: null } } } }
+      const newSelection1 = table.correctSelectionPos(start1, end1)
+
+      expect(newSelection1.length).toBe(1)
+      expect(
+        compareDocPos(newSelection1[0].start!, { index: 0, inner: { index: 0, inner: { index: 0, inner: null } } }),
+      ).toBe(0)
+      expect(
+        compareDocPos(newSelection1[0].end!, { index: 0, inner: { index: 0, inner: { index: 2, inner: null } } }),
+      ).toBe(0)
+
+      const start2 = { index: 0, inner: { index: 0, inner: { index: 0, inner: { index: 1, inner: null } } } }
+      const end2 = { index: 0, inner: { index: 0, inner: { index: 2, inner: { index: 1, inner: null } } } }
+      const newSelection2 = table.correctSelectionPos(start2, end2)
+
+      expect(newSelection2.length).toBe(1)
+      expect(compareDocPos(newSelection2[0].start!, { index: 0, inner: { index: 0, inner: null } })).toBe(0)
+      expect(compareDocPos(newSelection2[0].end!, { index: 0, inner: { index: 1, inner: null } })).toBe(0)
+    }
+  })
 })
